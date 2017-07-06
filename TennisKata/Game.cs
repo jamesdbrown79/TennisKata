a4401207fe7869337f63d7370e8550eeacd1fc85
ja7r76j <ja7r76j@L-UKC55FTS32>
2017-07-06 16:26:19 +0100

    Update 1

---
+++ TennisKata/Game.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TennisKata
+{
+    public class Game
+    {
+        private int player1points;
+        private int player2points;
+
+        public void Player1WinsPoint()
+        {
+            if (IsPlayer2Advantage())
+            {
+                player2points--;
+            }
+            else
+            {
+                player1points++;
+            }
+        }
+
+        public void Player2WinsPoint()
+        {
+            if (IsPlayer1Advantage())
+            {
+                player1points--;
+            }
+            else
+            {
+                player2points++;
+            }
+        }
+
+        public bool Player1Wins()
+        {
+            return (player1points >= 4) && (player1points - player2points >= 2);
+        }
+
+        public bool Player2Wins()
+        {
+            return (player2points >= 4) && (player2points - player1points >= 2);
+        }
+
+        public string Score()
+        {
+            if (IsDeuce())
+            {
+                return "deuce";
+            }
+            else if (IsAdvantage())
+            {
+                return ("advantage");
+            }
+            else
+            {
+                return TennisFormatScore(player1points) + "-" + TennisFormatScore(player2points);
+            }
+        }
+
+        private bool IsAdvantage()
+        {
+            return (player1points == 4 && player2points == 3) || (player1points == 3 && player2points == 4);
+        }
+
+        private bool IsPlayer1Advantage()
+        {
+            return player1points == 4 && player2points == 3;
+        }
+
+        private bool IsPlayer2Advantage()
+        {
+            return player2points == 4 && player1points == 3;
+        }
+
+        private bool IsDeuce()
+        {
+            return player1points == 3 && player2points == 3;
+        }
+
+        private string TennisFormatScore(int points)
+        {
+            switch (points)
+            {
+                case 1:
+                    return "15";
+                case 2:
+                    return "30";
+                case 3:
+                    return "40";
+                default:
+                    return "0";
+            }
+        }
+    }
+}
---
+++ TennisKataTests/GameTests.cs
@@ -0,0 +1,132 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TennisKata;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TennisKata.Tests
+{
+    [TestClass()]
+    public class GameTests
+    {
+        private Game game;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            game = new Game();
+        }
+
+        private void Player1WinsConsecutivePoints(int points)
+        {
+            for (int index = 1; index <= points; index++)
+            {
+                game.Player1WinsPoint();
+            }
+        }
+
+        private void Player2WinsConsecutivePoints(int points)
+        {
+            for (int index = 1; index <= points; index++)
+            {
+                game.Player2WinsPoint();
+            }
+        }
+
+        [TestMethod()]
+        public void should_return_player1_wins_game()
+        {
+            //setup
+
+            //act
+            Player1WinsConsecutivePoints(4);
+
+            //assert
+            Assert.AreEqual(true, game.Player1Wins());
+        }
+
+        [TestMethod()]
+        public void should_return_player1_wins_game_by_2_clear_points()
+        {
+            //setup
+
+            //act
+            Player1WinsConsecutivePoints(3);
+            Player2WinsConsecutivePoints(2);
+            game.Player1WinsPoint();
+
+            //assert
+            Assert.AreEqual(true, game.Player1Wins());
+        }
+
+        [TestMethod()]
+        public void should_return_player2_wins_game_by_2_clear_points()
+        {
+            //setup
+
+            //act
+            Player1WinsConsecutivePoints(2);
+            Player2WinsConsecutivePoints(4);
+
+            //assert
+            Assert.AreEqual(true, game.Player2Wins());
+        }
+
+        [TestMethod()]
+        public void should_return_game_score_using_tennis_terms()
+        {
+            //setup
+
+            //act
+            Player1WinsConsecutivePoints(2);
+            Player2WinsConsecutivePoints(3);
+
+            //assert
+            Assert.AreEqual("30-40", game.Score());
+        }
+
+        [TestMethod()]
+        public void should_return_deuce_when_both_players_score_3_points()
+        {
+            //setup
+
+            //act
+            Player1WinsConsecutivePoints(3);
+            Player2WinsConsecutivePoints(3);
+
+            //assert
+            Assert.AreEqual("deuce", game.Score());
+        }
+
+        [TestMethod()]
+        public void should_return_advantage_when_score_is_4_3()
+        {
+            //setup
+
+            //act
+            Player1WinsConsecutivePoints(3);
+            Player2WinsConsecutivePoints(3);
+            game.Player2WinsPoint();
+
+            //assert
+            Assert.AreEqual("advantage", game.Score());
+        }
+
+        [TestMethod()]
+        public void should_return_deuce_from_advantage_when_3_point_player_wins_point()
+        {
+            //setup
+
+            //act
+            Player1WinsConsecutivePoints(3);
+            Player2WinsConsecutivePoints(3);
+            game.Player2WinsPoint();
+            game.Player1WinsPoint();
+
+            //assert
+            Assert.AreEqual("deuce", game.Score());
+        }
+    }
+}
